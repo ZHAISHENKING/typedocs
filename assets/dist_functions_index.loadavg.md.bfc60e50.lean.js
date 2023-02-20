991756dbcd3910261be1847f48403b954ba5c4ef
shirenjie <shirenjie@coalmineinfo.cn>
2023-02-20 11:18:57 +0800

    deploy

--- assets/dist_functions_index.loadavg.md.bfc60e50.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o,a as n}from"./app.8ea3f0da.js";const g=JSON.parse('{"title":"Function: loadavg","description":"","frontmatter":{},"headers":[],"relativePath":"dist/functions/index.loadavg.md"}'),t={name:"dist/functions/index.loadavg.md"},d=n("",12),r=[d];function s(i,c,l,p,u,_){return o(),a("div",null,r)}const m=e(t,[["render",s]]);export{g as __pageData,m as default};
